f6921a7a638f43016cbc96e7f2cc9fc9b67dc952
Yuki992411 <k.yuki.9924.11@gmail.com>
2025-08-23 14:56:17 +0900

    first commit

---
+++ src/main/kotlin/com/example/yukikom/redisson_redis_cluster/RedissonRedisClusterApplication.kt
@@ -0,0 +1,11 @@
+package com.example.yukikom.redisson_redis_cluster
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RedissonRedisClusterApplication
+
+fun main(args: Array<String>) {
+	runApplication<RedissonRedisClusterApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/yukikom/redisson_redis_cluster/RedissonRedisClusterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.yukikom.redisson_redis_cluster
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedissonRedisClusterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
